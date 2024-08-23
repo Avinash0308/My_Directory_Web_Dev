edcd1b93ac43f196768596f7e2a89d6c1b1b778b
Avinash Agrawal <avinash03082003@gmail.com>
2024-08-24 01:26:42 +0530

    Started JS, Day 54 Added

---
+++ JS/Day54/script.js
@@ -0,0 +1,21 @@
+alert("Hello World");
+
+console.log("Hey I am console");
+console.log("Hey I am console");
+console.log("Hey I am console");
+
+var a = prompt("Enter your number");
+
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer");
+
+
+if(isTrue){
+    console.log("Blasting The Computer");
+}
+else{
+    console.log("You saved your computer");
+}
+console.log("Your Number is " + a);
+
+document.title = "Hey I am good";
+document.body.style.backgroundColor = "red";
